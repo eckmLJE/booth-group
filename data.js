192d35c78b2fb3a69a317eafadfbfe255a7852e8
Lucas Eckman <lucas.eckman@gmail.com>
2019-02-02 20:28:17 -0500

    setup complete

---
+++ data.js
@@ -0,0 +1,89 @@
+var venueBooths = [
+  {
+    id: 1,
+    x: 50,
+    y: 50,
+    w: 50,
+    h: 65,
+    reservation: {
+      orgName: "BTW",
+      orgId: 90
+    }
+  },
+  {
+    id: 2,
+    x: 150,
+    y: 50,
+    w: 50,
+    h: 65,
+    reservation: {
+      orgName: "IOU",
+      orgId: 7
+    }
+  },
+  {
+    id: 3,
+    x: 250,
+    y: 50,
+    w: 50,
+    h: 65,
+    reservation: {}
+  },
+  {
+    id: 4,
+    x: 50,
+    y: 150,
+    w: 50,
+    h: 65,
+    reservation: {}
+  },
+  {
+    id: 5,
+    x: 150,
+    y: 150,
+    w: 50,
+    h: 65,
+    reservation: {
+      orgName: "IMHO",
+      orgId: 25
+    }
+  },
+  {
+    id: 6,
+    x: 250,
+    y: 150,
+    w: 50,
+    h: 65,
+    reservation: {}
+  },
+  {
+    id: 7,
+    x: 50,
+    y: 245,
+    w: 50,
+    h: 65,
+    reservation: {
+      orgName: "FYI",
+      orgId: 25
+    }
+  },
+  {
+    id: 8,
+    x: 150,
+    y: 245,
+    w: 50,
+    h: 65,
+    reservation: {
+      orgName: "BTW",
+      orgId: 25
+    }
+  },
+  {
+    id: 9,
+    x: 250,
+    y: 245,
+    w: 50,
+    h: 65,
+    reservation: {}
+  }
+];
---
+++ index.js
@@ -0,0 +1,66 @@
+var svg = d3.select("#venue-svg");
+
+function zoomed() {
+  g.attr("transform", d3.event.transform);
+}
+
+var g = svg.append("g");
+
+var tables = g
+  .selectAll("g")
+  .data(venueBooths)
+  .enter()
+  .append("g")
+  .attr("transform", function(d) {
+    return "translate(" + d.x + " " + d.y + ")";
+  });
+
+var tableRects = tables
+  .append("rect")
+  .attr("stroke", "steelblue")
+  .attr("stroke-width", "2px")
+  .attr("width", function(d) {
+    return d.w;
+  })
+  .attr("height", function(d) {
+    return d.h;
+  })
+  .attr("fill", "none")
+  .attr("fill", function(d) {
+    return $.isEmptyObject(d.reservation) ? "none" : "#FF5733";
+  })
+  .attr("id", function(d) {
+    return "table-" + d.id;
+  });
+
+tables
+  .append("text")
+  .text(function(d) {
+    return "Booth " + d.id;
+  })
+  .attr("dx", 5)
+  .attr("dy", 60)
+  .attr("font-size", "8px");
+
+tables
+  .append("text")
+  .text(function(d) {
+    return d.reservation.orgName ? d.reservation.orgName : "Available";
+  })
+  .attr("dy", 15)
+  .attr("dx", 5)
+  .attr("font-size", "9px")
+  .attr("font-weight", "bold");
+
+svg
+  .append("rect")
+  .attr("width", "100%")
+  .attr("height", "100%")
+  .style("fill", "none")
+  .style("pointer-events", "all")
+  .call(
+    d3
+      .zoom()
+      .scaleExtent([1 / 2, 4])
+      .on("zoom", zoomed)
+  );
